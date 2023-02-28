b63ef3f364f74d8fa66b0bd0de91175140a9d314
Shailendra Singh Niboriya <shailniboriya@gmail.com>
2023-02-28 15:42:09 +0530

    Update hyperparams.py

--- hyperparams.py
+++ hyperparams.py
@@ -1,1 +1,22 @@
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
 
+import os
+
+# set hyperparameters
+EPOCHS = 25
+INIT_LR = 1e-3
+BS = 32
+default_image_size = tuple((256,256))
+image_size = 0
+#please change the location of  input folder according to your machine
+curr_dir= os.getcwd()
+directory_root = "./input/train/"
+out_dir = "./output/"
+width=256
+height=256
+depth=3
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
