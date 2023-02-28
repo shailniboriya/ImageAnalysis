bdebdb494f46d5e018d509da60f2894c68c6c420
Shailendra Singh Niboriya <shailniboriya@gmail.com>
2023-02-28 15:44:41 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,63 @@
+from hyperparams import *
+from data_prep import *
+
+import numpy as np
+import cv2
+from tensorflow import keras 
+from sklearn.model_selection import train_test_split
+
+# Pre Model
+
+np_image_list = np.array(image_list, dtype=np.float16) / 225.0
+
+print("[INFO] Spliting data to train, test")
+x_train, x_test, y_train, y_test = train_test_split(np_image_list, image_labels, test_size=0.2, random_state = 42) 
+
+# In[ ]:
+
+aug = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=25, width_shift_range=0.1,
+    height_shift_range=0.1, shear_range=0.2, 
+    zoom_range=0.2,horizontal_flip=True, 
+    fill_mode="nearest")
+
+# model architecture
+model = tf.keras.Sequential()
+inputShape = (height, width, depth)
+chanDim = -1
+if tf.keras.backend.image_data_format() == "channels_first":
+    inputShape = (depth, height, width)
+    chanDim = 1
+model.add(tf.keras.layers.Conv2D(32, (3, 3), padding="same",input_shape=inputShape))
+model.add(tf.keras.layers.Activation("relu"))
+model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(3, 3)))
+model.add(tf.keras.layers.Dropout(0.25))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), padding="same"))
+model.add(tf.keras.layers.Activation("relu"))
+model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), padding="same"))
+model.add(tf.keras.layers.Activation("relu"))
+model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
+model.add(tf.keras.layers.Conv2D(128, (3, 3), padding="same"))
+model.add(tf.keras.layers.Activation("relu"))
+model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+model.add(tf.keras.layers.Conv2D(128, (3, 3), padding="same"))
+model.add(tf.keras.layers.Activation("relu"))
+model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(1024))
+model.add(tf.keras.layers.Activation("relu"))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(n_classes))
+model.add(tf.keras.layers.Activation("softmax"))
+
+# In[ ]: Model Summary
+
+model.summary()
 
