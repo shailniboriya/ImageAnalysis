bb2a55ab695a4c3a35eadba0cfb9196b1ec70988
Shailendra Singh Niboriya <shailniboriya@gmail.com>
2023-04-05 14:32:15 +0530

    Update data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -23,39 +23,20 @@ def convert_image_to_array(image_dir):
 # In[ ]: Fetch images from directory
 
 image_list, label_list = [], []
-try:
-    print("[INFO] Loading images ...")
-    root_dir = listdir(directory_root)
-    for directory in root_dir :
-        # remove .DS_Store from list
-        if directory == ".DS_Store" :
-            root_dir.remove(directory)
 
-    for superclass_folder in root_dir :
-        class_name_folder_list = listdir(f"{directory_root}/{superclass_folder}")
-        
-        for class_folder in class_name_folder_list :
-            # remove .DS_Store from list
-            if class_folder == ".DS_Store" :
-                class_name_folder_list.remove(class_folder)
-
-        for class_name_folder in class_name_folder_list:
-            print(f"[INFO] Processing {class_name_folder} ...")
-            class_name_image_list = listdir(f"{directory_root}/{superclass_folder}/{class_name_folder}/")
-                
-            for single_class_name_image in class_name_image_list :
-                if single_class_name_image == ".DS_Store" :
-                    class_name_image_list.remove(single_class_name_image)
-
-            for imge in class_name_image_list[:200]:
-                image_directory = f"{directory_root}/{superclass_folder}/{class_name_folder}/{imge}"
-                if image_directory.endswith(".jpg") == True or image_directory.endswith(".JPG") == True:
-                    image_list.append(convert_image_to_array(image_directory))
-                    label_list.append(class_name_folder)
-    print("[INFO] Image loading completed")  
-except Exception as e:
-    print(f"Error : {e}")
+print("[INFO] Loading images ...")
+class_name_folder_list = listdir(directory_root)
+   
+for class_name_folder in class_name_folder_list:
+    print(f"[INFO] Processing {class_name_folder} ...")
+    class_name_image_list = listdir(f"{directory_root}/{class_name_folder}/")     
     
+    for imge in class_name_image_list[:200]:
+        file = f"{directory_root}/{class_name_folder}/{imge}"
+        if file.endswith(".jpg") == True or file.endswith(".JPG") == True or file.endswith(".jpeg") == True or file.endswith(".JPEG") == True or file.endswith(".png") == True or file.endswith(".PNG") == True:
+            image_list.append(convert_image_to_array(file))
+            label_list.append(class_name_folder)
+print("[INFO] Image loading completed")
 
 # data preprocessing
 # In[ ]:Get Size of Processed Image
